2a0eee020015d3656e75743941f2f03a5e22b405
Cesar <jcchoque23@gmail.com>
2026-05-24 21:34:28 -0400

    complicandonos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,11 @@
+@extends('layouts.base')
+
 <form class="mt-14 space-y-5" novalidate>
     <div class="flex flex-col gap-2">
         <label class="font-bold text-2xl" for="email">Email</label>
 
-        <input 
-            id="email" 
-            type="email" 
-            placeholder="Email de Registro"
-            class="w-full border border-gray-300 p-3 rounded-lg" 
-            name="email" 
-            tabindex="1" 
-        />
+        <input id="email" type="email" placeholder="Email de Registro"
+            class="w-full border border-gray-300 p-3 rounded-lg" name="email" tabindex="1" />
     </div>
 
     <div class="flex flex-col gap-2">
@@ -17,17 +13,10 @@
             <label class="font-bold text-2xl">Password</label>
             <a href="#" class="text-indigo-950" tabindex="3">¿Olvidaste tu Contraseña?</a>
         </div>
-        <input 
-            type="password" 
-            placeholder="Password de Registro" 
-            class="w-full border border-gray-300 p-3 rounded-lg"
-            name="password" 
-            tabindex="2" 
-        />
+        <input type="password" placeholder="Password de Registro" class="w-full border border-gray-300 p-3 rounded-lg"
+            name="password" tabindex="2" />
     </div>
-    <input 
-        type="submit" 
-        value='Iniciar Sesión'
-        class="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-bold  text-xl cursor-pointer" 
-    />
+    <input type="submit" value='Iniciar Sesión'
+        class="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-bold  text-xl cursor-pointer" />
 </form>
+@endsection
