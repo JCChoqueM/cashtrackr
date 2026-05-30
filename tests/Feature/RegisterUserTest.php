77ef0e906487084691569e2f848181a45ca90030
Cesar <jcchoque23@gmail.com>
2026-05-30 03:26:16 -0400

    71  test al formulario cuando esta vacio

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
 
 uses(RefreshDatabase::class);
 it('shows registration form', function () {
@@ -19,6 +22,8 @@ it('shows registration form', function () {
 });
 
 it('registers a new user as unverified and dispatches the registered event', function () {
+    Event::fake();
+
 
     $response = $this->post(route('register.store'), [
         'name' => 'Juan Perez',
@@ -28,4 +33,27 @@ it('registers a new user as unverified and dispatches the registered event', fun
     ]);
 
     $response->assertRedirect(route('verification.notice'));
+    $user = User::where('email', 'prueba@prueba.com')->first();
+
+    expect($user)->not()->toBeNull();
+    expect($user->name)->toBe('Juan Perez');
+    expect($user->email)->toBe('prueba@prueba.com');
+    expect($user->hasVerifiedEmail())->toBeFalse();
+
+    Event::assertDispatched(Registered::class);
+});
+
+it('should validate required fields when the request body is empty', function () {
+    $response = $this->post(route('register.store'), []);
+
+    $response->assertSessionHasErrors([
+        'name',
+        'email',
+        'password',
+    ]);
+    $response->assertSessionHasErrors([
+        'name' => 'El Nombre es obligatorio',
+        'email' => 'El E-mail es obligatorio',
+        'password' => 'La Contraseña es obligatorio',
+    ]);
 });
