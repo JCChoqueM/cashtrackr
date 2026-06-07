1d34baef36f3bbafb211ed091d006e730e1c97f7
Cesar <jcchoque23@gmail.com>
2026-06-06 22:34:42 -0400

    añadido migracion

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Expense;
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+
+
+  
+    public function store(Request $request)
+    {
+        //
+    }
+    
+    public function update(Request $request, Expense $expense)
+    {
+        //
+    }
+
+    
+    public function destroy(Expense $expense)
+    {
+        //
+    }
+}
